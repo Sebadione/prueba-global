67a5905962ffda05ea965a6c3270cc700641824a
Sebastián Emiliano Dionicio <92940@sistemas.frc.utn.edu.ar>
2024-11-21 12:36:10 -0300

    redirects

---
+++ src/api.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
+
+
+@ApiExcludeController()
+@Controller('/')
+export class ApiController {
+  @Get()
+  getApiInfo() {
+    return {
+      message: 'Bienvenido a la API',
+      availableEndpoints: {
+        users: '/api/v1/users',
+        docs: '/api-docs',
+      },
+      status: 200,
+    };
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ApiController } from './api.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://root:root@cluster0.x6c8j.mongodb.net/db_global'),
     UsersModule,
   ],
+  controllers: [ApiController],
 })
 
 export class AppModule {}
